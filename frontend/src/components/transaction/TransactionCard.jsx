5cc9d5f2e98aed40525ec832b38e3264bdbdfafe
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-05-28 01:46:00 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   frontend/src/components/transaction/TransactionCard.jsx

--- frontend/src/components/transaction/TransactionCard.jsx
+++ frontend/src/components/transaction/TransactionCard.jsx
@@ -1,6 +1,24 @@
 import formatCurrency from "../../utils/formatCurrency";
+import { FaRegTrashAlt } from "react-icons/fa";
+import useTransactions from "../../hooks/useTransactions";
 
 const TransactionCard = ({ transaction }) => {
+
+    const { removeTransaction } = useTransactions();
+
+    const handleDelete = () => {
+
+        const confirmDelete = window.confirm(
+            `Delete "${transaction.title}" transaction?`
+        );
+
+        if (confirmDelete) {
+            removeTransaction(transaction._id);
+            
+        }
+    };
+
+
     return (
         <div className="p-3 border rounded flex justify-between">
             <div>
@@ -10,8 +28,19 @@ const TransactionCard = ({ transaction }) => {
                 </p>
             </div>
 
-            <div className={transaction.type === "income" ? "text-green-500" : "text-red-500"}>
+            <div className={`${transaction.type === "income" ? "text-green-500" : "text-red-500"} font-bold flex items-center gap-3`}>
                 {formatCurrency(transaction.amount)}
+
+                <div>
+
+                    <button
+                        onClick={handleDelete}
+                        className="text-red-500 hover:text-red-700"
+                    >
+                        <FaRegTrashAlt size={25} />
+                    </button>
+
+                </div>
             </div>
         </div>
     );
