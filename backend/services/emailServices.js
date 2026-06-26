03a75b214df0087ab26ecf9551d94fb4ea59832d
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-06-26 10:05:31 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   backend/services/emailServices.js

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -5,8 +5,9 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
