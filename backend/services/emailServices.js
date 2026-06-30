a99af35ef76227ff0fc8da5683b2a1c70f105794
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-06-30 07:32:13 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   backend/services/emailServices.js

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -4,6 +4,9 @@ const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
+    requireTLS: true,
+    logger: true,
+    debug: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
